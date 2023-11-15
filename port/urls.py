1a82485c0e9249f22fd43b8dcba252c928f4b211
Gulshan kumar <121786265+Gulshanray@users.noreply.github.com>
2023-11-15 15:14:32 +0530

    Update urls.py

--- port/urls.py
+++ port/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from folio import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('list/', views.index, name='list'),
 
     
