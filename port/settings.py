c782e0b26e400ace788233754d08a0d976e1461e
Gulshan kumar <121786265+Gulshanray@users.noreply.github.com>
2023-11-16 15:47:15 +0530

    Update settings.py

--- port/settings.py
+++ port/settings.py
@@ -131,23 +131,9 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-# settings.py
-
-# ...
-
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# ... other settings ...
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 # Default primary key field type
